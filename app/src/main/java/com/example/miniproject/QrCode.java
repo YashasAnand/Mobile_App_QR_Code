0cbb9a01ae2644e85c905099ae1a957f1bb023f4
Yash Aryan Chopra <yasharyanchopra2@gmail.com>
2021-07-05 20:24:04 +0530

    Added Create QR Functionality, created homepage

--- app/src/main/java/com/example/miniproject/QrCode.java
+++ app/src/main/java/com/example/miniproject/QrCode.java
@@ -2,13 +2,39 @@ package com.example.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QrCode extends AppCompatActivity {
+
+    Button createQrBtn, scanQrBtn;
+    TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code);
+
+        createQrBtn = findViewById(R.id.createQrBtn);
+        scanQrBtn = findViewById(R.id.scanQrBtn);
+        welcomeText = findViewById(R.id.welcomeText);
+
+        createQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createIntent = new Intent(QrCode.this, createQrActivity.class);
+                startActivity(createIntent);
+            }
+        });
+
+        scanQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
